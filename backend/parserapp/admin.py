e779922054723aa45124c6b4afac22f89895a5d7
Oleh Olinyk <oleh.olinyk.oo@gmail.com>
2024-07-29 17:41:01 +0200

    upd
    upd task for parser and configure admin.py

--- backend/parserapp/admin.py
+++ backend/parserapp/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
 from .models import PostLink, Post
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+import json
 
-@admin.register(PostLink)
-class PostLinkAdmin(admin.ModelAdmin):
-    list_display = ('url', 'created_at')
+def setup_periodic_tasks():
+    schedule, created = IntervalSchedule.objects.get_or_create(
+        every=1,
+        period=IntervalSchedule.HOURS,
+    )
 
-@admin.register(Post)
+    PeriodicTask.objects.create(
+        interval=schedule,
+        name='Parse links every hour',
+        task='parserapp.tasks.parse_links',
+    )
+
+    PeriodicTask.objects.create(
+        interval=schedule,
+        name='Parse content every hour',
+        task='parserapp.tasks.parse_content',
+    )
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'processed')
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        setup_periodic_tasks()
+
+admin.site.register(PostLink)
+admin.site.register(Post, PostAdmin)
--- backend/parserapp/tasks.py
+++ backend/parserapp/tasks.py
@@ -1,18 +1,11 @@
 from celery import shared_task
-from .models import PostLink, Post
-from .utils import urlNewsPars, urlTextPars
+from .utils.urlNewsPars import listLinkParser
+from .utils.urlTextPars import urlTextPars
 
 @shared_task
-def parse_links_task():
-    links = urlNewsPars()
-    for link in links:
-        PostLink.objects.create(url=link)
+def parse_links():
+    return listLinkParser()
 
 @shared_task
-def parse_content_task():
-    links = PostLink.objects.filter(processed=False)
-    for link in links:
-        content = urlTextPars(link.url)
-        Post.objects.create(title=content['title'], content=content['body'])
-        link.processed = True
-        link.save()
+def parse_content():
+    return urlTextPars()
