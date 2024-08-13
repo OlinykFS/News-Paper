a333d5a12834b26784a360c35a36bff80a837957
Oleh Olinyk <oleh.olinyk.oo@gmail.com>
2024-08-13 20:27:48 +0200

    upd
    admin and dockerFile Front update

--- backend/blogApp/admin.py
+++ backend/blogApp/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'content')
+    list_editable = ('title',) 
+    list_display_links = ('author', 'created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {'fields': ('title', 'content')}),
+        ('Author Information', {'fields': ('author',)}),
+        ('Dates', {'fields': ('created_at', 'updated_at')}),
+    )
+
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(Post, PostAdmin)
